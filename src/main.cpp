a7d611b72113f5ea1e181e976ee345d7a3471b56
bladez-fate <bladez.entertainment@gmail.com>
2017-06-30 14:40:40 -0700

    added project thet let you enjoy the black screen

---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+// The MIT License(MIT)
+//
+// Copyright 2017 bladez-fate
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#include "pilecode.h"
+
+#include "engine/easy.h"
+
+using namespace arctic;  // NOLINT
+using namespace arctic::easy;  // NOLINT
+
+Sprite g_robot;
+
+void Init()
+{
+	//g_robot.Load("data/robot.tga");
+
+	ResizeScreen(640, 400);
+}
+
+void Render()
+{
+	Clear();
+	ShowFrame();
+}
+
+void EasyMain()
+{
+	Init();
+	while (true) {
+		if (IsKey(kKeyEscape)) {
+			break;
+		}
+		Render();
+	}
+}
